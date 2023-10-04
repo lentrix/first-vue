cd722eb9e20c7cf3e317d3f643cacdccd4e0b1e0
lentrix <hawkmanlentrix@gmail.com>
2023-10-04 15:52:11 +0800

    init

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <img :src="person.image" alt="">
+        <div class="person-details">
+            <div>
+                <strong>Last Name</strong> {{person?.lastName}}
+            </div>
+            <div>
+                <strong>First Name</strong> {{person?.firstName}}
+            </div>
+            <div>
+                <strong>Email</strong> {{ person?.email }}
+            </div>
+        </div>
+        <div class="buttons">
+            <SpecialButton style="flex: 1" title="Like" type="primary" />
+            <SpecialButton style="flex: 1" title="Dislike" type="danger" />
+        </div>
+    </div>
+</template>
+
+<script>
+import SpecialButton from './SpecialButton.vue';
+
+export default {
+    props: {
+        person: Object
+    },
+    components: {
+        SpecialButton,
+    }
+}
+
+</script>
+
+<style scoped>
+img {
+    width: 100%;
+}
+
+.buttons {
+    display: flex;
+    gap: 4px;
+}
+.container {
+    max-width: 200px;
+    border: 1px solid #888;
+    padding: 12px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 5px #888;
+    margin: 16px 0;
+}
+
+</style>
---
+++ src/components/SpecialButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="type">
+        {{title}}
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: String,
+        type: String //primary, danger, warning, success
+    }
+}
+
+</script>
+
+<style scoped>
+.primary {
+    background-color: #2686e7;
+    border-color: #06386a;
+}
+.danger {
+    background-color: #e72636;
+    border-color: #6a0609;
+}
+.warning {
+    background-color: #e76326;
+    border-color: #6a1806;
+}
+.success {
+    background-color: #11c839;
+    border-color: #045e22;
+}
+div {
+    padding: 4px;
+    color: white;
+    border-radius: 4px;
+    border: 4px solid;
+    margin: 8px 0;
+    text-align: center;
+}
+</style>
